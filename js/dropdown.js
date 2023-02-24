b2c9b598b7f97d8adce797e0b7b1a46f2759e97b
MalikKtK <mali0237@stud.kea.dk>
2023-02-24 09:54:05 +0100

    houjpk l låm

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,25 +1,41 @@
 console.log("dropdown.js loaded");
 const fruits = []
-fruits.push({"name": "Æble", "style": "fruit"})
-fruits.push({"name": "Pære", "style": "fruit"})
-fruits.push({"name": "Peanut", "style": "fruit"})
-fruits.push({"name": "Appelsin", "style": "fruit"})
+fruits.push({"id": 1, "name": "Æble", "style": "fruit"})
+fruits.push({"id": 2, "name": "Pære", "style": "fruit"})
+fruits.push({"id": 3, "name": "Peanut", "style": "nut"})
+fruits.push({"id": 4, "name": "Appelsin", "style": "fruit"})
 
 console.log(fruits);
 
-const pbFillDropDown = document.getElementById("pbFillDropDown");
-const ddFruits = document.getElementById("ddFruits");
+const pbFillDropDown = document.getElementById("pbFillDropDown")
+const ddFruits = document.getElementById("ddFruits")
 
 function addFruitToDropDown(item) {
     console.log(item)
     const el = document.createElement("option")
     el.textContent = item.name
     ddFruits.appendChild(el)
+    // el.value = item.id , put primarykey in value
+    el.value = item //Why not put object in value
+    el.className = ""
+    el.classList.add(item.style)
+    el.classList
+    el.className = item.style
+
 }
 
 function fillDropDown() {
     fruits.forEach(addFruitToDropDown)
 }
 
-pbFillDropDown.addEventListener("click", fillDropDown);
+function selectFruit() {
+    const selindex = ddFruits.selectedIndex
+    const selectedFruit = ddFruits.options[selindex]
+    console.log(selindex)
+    console.log(selectedFruit)
+    console.log(selectedFruit.value)
+}
+
+pbFillDropDown.addEventListener('click', fillDropDown)
+ddFruits.addEventListener('change', selectFruit)
 
